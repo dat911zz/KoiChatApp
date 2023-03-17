421493ff9f656954b37500d6c9d2c3b650af3948
datvn <datcy2011@gmail.com>
2023-03-17 23:18:04 +0700

    update login form

--- src/main/java/com/mycompany/cakoiphieuluuky/App.java
+++ src/main/java/com/mycompany/cakoiphieuluuky/App.java
@@ -7,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 /**
  * JavaFX App
@@ -17,9 +20,31 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        //Khởi tạo fxml Login và setup stage
+        scene = new Scene(loadFXML("Login"), 640, 400);
         stage.setScene(scene);
+        stage.setTitle("Hệ thống quản trị BankNSolution");
         stage.show();
+        //Gọi các thành phần view
+        TextField txtName = (TextField) scene.lookup("#txtName");
+        PasswordField txtPass = (PasswordField) scene.lookup("#txtPass");
+        Button btnSubmit = (Button) scene.lookup("#loginBtn");
+        //Bắt event thông qua code
+        btnSubmit.setOnAction((event) -> {    // lambda expression
+            System.out.println("Clicked!\n");
+            System.out.println("Name: " + txtName.getText() + "\nPassword: " + txtPass.getText());
+            if (!txtName.getText().equals("") && !txtPass.getText().equals("")) {
+                try {
+                    //Mở một fxml khác
+                    App.setRoot("secondary");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+            else{
+                System.out.println("Error: All field are required!");
+            }
+        });
     }
 
     static void setRoot(String fxml) throws IOException {
---
+++ src/main/java/com/mycompany/cakoiphieuluuky/LoginController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.cakoiphieuluuky;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author datcy
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        System.out.println("App: " + url);
+    }    
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
